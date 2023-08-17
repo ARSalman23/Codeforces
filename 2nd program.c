b23fd4f530ef9811ce90e67e3606d1a3e8ff0381
Md Abdur Rahman Salman <139158260+ARSalman23@users.noreply.github.com>
2023-08-17 14:33:05 +0600

    Add files via upload
    https://codeforces.com/problemset/problem/1859/F

---
+++ 2nd program.c
@@ -0,0 +1,34 @@
+/*
+#include<math.h>
+int main()
+{
+int a=5;
+int b=2;
+int power= pow(a,b);
+printf("%d", power);
+return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+int a=5, b= 5;
+int sum= a+b;
+int multiply= a*b;
+
+printf("%d\n%d\n", sum, multiply);
+
+return 0;
+}
+
+/*
+#include <math.h>   // Include the math.h library for mathematical functions
+int main() {
+    int a = 5;      // Initialize the variable 'a' with the value 5
+    int b = 2;      // Initialize the variable 'b' with the value 2
+    int power = pow(a, b);  // Calculate 'a' raised to the power of 'b' using the pow function
+    printf("%d", power);    // Print the result of the power calculation
+    return 0;       // Return 0 to indicate successful program execution
+}
+
+*/
